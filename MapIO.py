ac131e741b755ac38050630b8735ab9e64ddf906
Nathan <nathanminer923@gmail.com>
2025-05-12 20:11:23 -0500

    really good optimization

--- MapIO.py
+++ MapIO.py
@@ -1,4 +1,11 @@
-import yaml
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import sys
+import numpy as np
+
+from PIL import Image
+from PIL.ImageQt import ImageQt
 
 byte_data = open("split/map.bin", "rb").read()
 
@@ -18,7 +25,6 @@ curBank = 0
 for offset in banksMap:
     for i in range(0x2000):
         tileOffs = offset + i #Offset of current 64x64 map data
-        arrayOffs = (curBank * 0x2000) + i
         currentByte = byte_data[tileOffs]
         mapTiles.append(currentByte & 0b00111111); #Get lower 6 bits only of map data, store in array
         mapTileset.append((currentByte & 0b01000000) != 0)
@@ -32,7 +38,6 @@ curBank = 0
 for offset in banksSector:
     for i in range(0x200):
         tileOffs = offset + (i * 4) #Offset of current 256x256 sector data
-
         #First 6 bits of each byte
         sectorPalette.append(byte_data[tileOffs] & 0b00111111)
         sectorArea.append(byte_data[tileOffs+1] & 0b00111111)
@@ -40,19 +45,31 @@ for offset in banksSector:
         sectorTileset2.append(byte_data[tileOffs+3] & 0b00111111)
     curBank += 1
 
-from PIL import Image
+color_map = {
+    (0,0,0,255): 0,
+    (102,102,102,255): 1,
+    (173,173,173,255): 2,
+    (255,254,255,255): 3,
+}
 
 graphics8 = []
 for tileset in range(32):
-    image = Image.open(f"extract/graphics/tileset{tileset+1}.png")
+    image = Image.open(f"extract/graphics/tileset{tileset+1}.png", 'r').convert("RGBA")
     for y in range(4):
         for x in range(0x10):
-            newImage = image.crop((x*8, y*8, (x+1)*8, (y+1)*8))
-            graphics8.append(newImage)
+            rect = (x*8, y*8, (x+1)*8, (y+1)*8)
+            newImage = np.array(image.crop(rect)).flatten()
+            bits = bytearray()
+            for i in range(len(newImage)//4):
+                color = tuple(newImage[i*4:(i+1)*4])
+                bits.append(color_map[color])
+
+            graphics8.append(bits)
 
 #Lower 6 bits of 3000-37FF
 graphics16 = []
 banks16 = [0x1000, 0x5000, 0x9000, 0xD000, 0x11000, 0x15000, 0x19000, 0x1D000]
+palettes64 = []
 curBank = 0
 for offset in banks16:
     for i in range(0x200):
@@ -66,14 +83,9 @@ for offset in banks16:
             graphics8[tileset + (byte_data[tileOffs+3] & 0b00111111)],
             i & 0b01111111
         ])
-    curBank += 1
-
-palettes64 = []
-curBank = 0
-for offset in banks16:
     for i in range(0x100):
         paletteOffs = offset + (i * 0x10); #Offset of current 64x64 palette data
-        new_palette = []
+        new_palette = bytearray()
         for j in range(0x10):
             value = (byte_data[paletteOffs + j] & 0b11000000) >> 6
             new_palette.append(value)
@@ -88,17 +100,17 @@ for offset in banks64:
     for i in range(0x100):
         tileset = (curBank * 4) + (i // 64); #4 tilesets for each bank
         tileOffs = offset + (i * 16); #Offset of current 64x64 palette data
-        curTiles = [0]*16 #Tile16's
+        curTiles = [] #Tile16's
         altTileset = []
-        tileNums = [0]*16
+        tileNums = []
 
         #Iterate through the 64x64 tile data & decode it
         for j in range(0x10):
             subOffs = tileOffs + j
             tileNum = byte_data[subOffs] & 0b01111111
             #This gets the correct 16x16 tile from the loaded list (ignoring all that alternate tileset crap)
-            curTiles[j] = graphics16[(tileset * 128) + tileNum]
-            tileNums[j] = tileNum
+            curTiles.append(graphics16[(tileset * 128) + tileNum])
+            tileNums.append(tileNum)
 
             if (byte_data[subOffs] & 0b10000000): #If last bit is set, use alternate tileset
                 altTileset.append(j)
@@ -119,20 +131,9 @@ for i in range(len(palette_data)//4):
     #fucks up some colors makes some good
     data = palette_data[i*4:(i+1)*4]
     data2 = data[2] & 0b00111111
-    if data[2] > data2:
+    if (data[2] & 0b11000000 > 0):
         data2 = 0x30
     palettes.append([15, data[1], data2, data[3]])
-
-
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-import sys, os, yaml
-from copy import deepcopy
-import numpy as np
-
-from PIL import Image, ImageOps
-from PIL.ImageQt import ImageQt
 
 NES_PALETTE = open("sheets/nes.pal", "rb").read()
 
@@ -159,37 +160,25 @@ for i in range(len(palettes)):
         palettes[i][color] = (id2[0], id2[1], id2[2])
 ##
 
-def palette_image(image, palette):
-    data = np.array(image)   # "data" is a height x width x 4 numpy array
-    red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
+def palette_image(data, palette):
+    lookup_table = np.array(palette, dtype=np.uint8)
+    colored_image = lookup_table[data]
+    colored_image.shape = (8,8,3)
+    return colored_image
 
-    # Replace white with red... (leaves alpha values alone...)
-    #transparent_areas = (red == 0) & (green == 0) & (blue == 0) & (alpha == 0)
-    black_areas = (red == 0) & (green == 0) & (blue == 0) & (alpha == 255)
-    gray_areas = (red == 102) & (green == 102) & (blue == 102) & (alpha == 255)
-    lgray_areas = (red == 173) & (green == 173) & (blue == 173) & (alpha == 255)
-    white_areas = (red == 255) & (green == 254) & (blue == 255) & (alpha == 255)
-
-    data[..., :-1][black_areas.T] = palette[0] # Transpose back needed
-    data[..., :-1][gray_areas.T] = palette[1] # Transpose back needed
-    data[..., :-1][lgray_areas.T] = palette[2] # Transpose back needed
-    data[..., :-1][white_areas.T] = palette[3] # Transpose back needed
-
-    return Image.fromarray(data)
-
-template = Image.new("RGBA", (1,1))
+template = None
 
 class Chunk(object):
-    def __init__(self, id):
+    def __init__(self, id, x, y):
         self.visible = False
         self.id = id
-        self.pixmap : QGraphicsPixmapItem = QGraphicsPixmapItem(QPixmap.fromImage(ImageQt(template)))
+        global template
+        self.pixmap : QGraphicsPixmapItem = QGraphicsPixmapItem(template)
+        self.pixmap.setPos(x*64,y*64)
         self.generated = False
 
     def generate_pixmap(self):
         ##make_chunk
-        newImage = Image.new("RGBA", (64, 64))
-
         global mapTileset, mapTiles
         global graphics64, graphics16, graphics8
         global sectorTileset1, sectorTileset2
@@ -213,36 +202,52 @@ class Chunk(object):
 
         g64_id = (ts1[sector_i] * 64) + mapTiles[self.id]
 
-        curTile = deepcopy(graphics64[g64_id])
+        curTile = graphics64[g64_id]
         for index in curTile[2]: #2 == altTileset
             curTile[0][index] = graphics16[(ts2[sector_i] * 128) + curTile[3][index]] #3 == tileNums
 
         palette_i = sectorPalette[sector_i]
-
         use_bg_palette = palettes[palette_i*4:(palette_i+1)*4]
+
+        #newImage = Image.new("RGBA", (64, 64))
+
+        pixels = {}
 
         for tile16_i in range(len(curTile[0])):
             tile16 = curTile[0][tile16_i]
             x16 = tile16_i % 4
             y16 = tile16_i // 4
             for tile8_i in range(4):
-                tile8 = tile16[tile8_i]
+                tile8 = tile16[tile8_i].copy()
 
                 x8 = tile8_i % 2
                 y8 = tile8_i // 2
                 x8 += (x16*2)
                 y8 += (y16*2)
 
+                i = x8 + (y8*8)
+
                 paletteNum = curTile[1][tile16_i]
                 my_palette = use_bg_palette[paletteNum]
 
                 new_tile = palette_image(tile8, my_palette)
-                rect = (x8*8, y8*8, (x8+1)*8, (y8+1)*8)
-                newImage.paste(new_tile, rect)
+                pixels[i] = new_tile
+                new_tile = None
+
+        p_array = []
+        for i in range(64):
+            p_array.append(pixels[i])
+
+        s = []
+        for i in range(8):
+            x = p_array[i*8:(i+1)*8]
+            s.append(np.hstack(x))
+        newImage = Image.fromarray(np.vstack(s)).convert("RGBA")
+
 
         self.pixmap.setPixmap(QPixmap.fromImage(ImageQt(newImage)))
-        self.pixmap.update()
         self.generated = True
+        newImage = None
 
 
 class Scene(QGraphicsScene):
@@ -254,16 +259,9 @@ class Scene(QGraphicsScene):
         for y in range(0xE0):
             for x in range(0x100):
                 i = x + (y * 0x100)
-                new_chunk = Chunk(i)
+                new_chunk = Chunk(i, x, y)
                 self.chunks.append(new_chunk)
-                new_chunk.pixmap.setPos(x*64,y*64)
                 self.addItem(new_chunk.pixmap)
-
-        print(len(self.items()))
-
-        self.queue_update()
-
-        print("DONE")
 
     def queue_update(self):
         if self.window == None:
@@ -287,7 +285,6 @@ class Scene(QGraphicsScene):
                 chunk : Chunk = self.chunks[i]
                 if not chunk.generated:
                     chunk.generate_pixmap()
-        self.update()
 
 
 class Viewer(QGraphicsView):
@@ -309,13 +306,14 @@ class Viewer(QGraphicsView):
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("MOTHER Animation Creator")
+        self.setWindowTitle("MOTHER map viewer")
         self.viewer = Viewer()
 
         self.setCentralWidget(self.viewer)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    template = QPixmap.fromImage(ImageQt(Image.new("RGBA", (1,1))))
     window = Window()
     window.show()
     sys.exit(app.exec())
